b8f219e883cd7507c84893a139ee3c96e6ba859a
Baldev Singh <personalemailofbaldev@gmail.com>
2024-10-14 12:07:44 +0530

    making neovim mappings.lua

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -13,6 +13,7 @@ _: {
 
     extraLuaConfig = ''
       ${builtins.readFile ./options.lua}
+      ${builtins.readFile ./mappings.lua}
     '';
   };
 }
---
+++ home-manager/neovim/mappings.lua
@@ -0,0 +1,16 @@
+vim.keymap.set("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+vim.keymap.set("i", "<C-e>", "<End>", { desc = "move end of line" })
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "move left" })
+vim.keymap.set("i", "<C-l>", "<Right>", { desc = "move right" })
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "move down" })
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "move up" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+
+vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
+
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
+vim.keymap.set("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
