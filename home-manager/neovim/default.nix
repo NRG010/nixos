9f203954fc888e615528253378ea9b6f20feedec
Baldev Singh <personalemailofbaldev@gmail.com>
2024-10-14 12:04:20 +0530

    making neovim option.lua

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -1,5 +1,18 @@
 _: {
-  programs.neovim = {
+  programs.neovim = 
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
     enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./options.lua}
+    '';
   };
 }
