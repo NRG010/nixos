07c3a50ab59cb54ebde91d448e6561a35503dd52
Baldev Singh <personalemailofbaldev@gmail.com>
2024-10-28 00:23:24 +0530

    adding xdg

--- configuration/packages.nix
+++ configuration/packages.nix
@@ -1,4 +1,5 @@
 { lib, pkgs, ... }: {
+
   environment.systemPackages = with pkgs; [
     obsidian
     polkit_gnome
@@ -7,14 +8,31 @@
     networkmanagerapplet
     networkmanager-openvpn
   ];
+
   nixpkgs.config.allowUnfreePredicate = pkg:
+
   builtins.elem (lib.getName pkg) [
     "obsidian"
     "widevine-cdm"
   ];
+
   nixpkgs.overlays = [
     (final: prev: { qutebrowser = prev.qutebrowser.override { enableWideVine = true; }; })
   ];
+
+  # XDG Portals
+  xdg = {
+    autostart.enable = true;
+    portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal
+        pkgs.xdg-desktop-portal-gtk
+      ];
+    };
+  };
+
   programs.nh.enable = true;
+
   environment.sessionVariables.FLAKE = "/etc/nixos";
 }
