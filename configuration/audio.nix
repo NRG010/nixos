9eeab374477922110b7b435783797792259ce372
Baldev Singh <personalemailofbaldev@gmail.com>
2024-11-03 15:57:02 +0530

    03/11/24 15:11

--- configuration/audio.nix
+++ configuration/audio.nix
@@ -1,4 +1,7 @@
-_: {
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    bluez
+  ];
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -8,5 +11,19 @@ _: {
     pulse.enable = true;
     jack.enable = true;
   };
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+	Experimental = true;
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+  systemd.user.services.mpris-proxy = {
+      description = "Mpris proxy";
+      after = [ "network.target" "sound.target" ];
+      wantedBy = [ "default.target" ];
+      serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
 }
