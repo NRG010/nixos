e9e003bebe35deb10fd58547a9af1357d9950dd2
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-24 16:42:59 +0530

    24/12/24-16:12

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, stylix, nvchad4nix, ...}: 
+  outputs = { self, nixpkgs, home-manager, stylix, nvchad4nix, ...}@inputs:
   let
     system = "x86_64-linux";
     lib = nixpkgs.lib;
