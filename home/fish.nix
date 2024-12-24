7e2df7b520dd043be14ce8ccd00b8d8b9489b7ed
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-24 21:05:15 +0530

    $(date +%d/%m/%y-%H:%m)

--- home/fish.nix
+++ home/fish.nix
@@ -9,13 +9,13 @@
       nr = "sudo nixos-rebuild switch --flake ~/.nix-config/#nixos";
       gb = 'git add -A;git commit -am "$(date +%d/%m/%y-%H:%m)";git push';
     };
-    loginShellInit = """
+    loginShellInit = ''
       if status is-login
-        if test -z "\$DISPLAY" -a "\$XDG_VTNR" = 1
+        if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
           Hyprland
         end
       end
       starship init fish | source
-    """;
+    '';
   };
 }
