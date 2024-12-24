ca0d3f9953585259fc89478daefeb2324cf191aa
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-24 20:58:53 +0530

    $(date +%d/%m/%y-%H:%m)

--- home/fish.nix
+++ home/fish.nix
@@ -11,7 +11,7 @@
     };
     loginShellInit = """
       if status is-login
-        if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        if test -z "\$DISPLAY" -a "\$XDG_VTNR" = 1
           Hyprland
         end
       end
