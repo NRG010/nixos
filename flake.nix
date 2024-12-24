c1c286992040a0593614339425c473b582cb4706
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-24 16:52:19 +0530

    24/12/24-16:12

--- flake.nix
+++ flake.nix
@@ -8,18 +8,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix.url = "github:danth/stylix";
-    nvchad4nix = {
-      url = "github:nix-community/nix4nvchad";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, stylix, nvchad4nix, ...}@inputs:
+  outputs = { self, nixpkgs, home-manager, stylix, nvchad4nix, ...}:
   let
     system = "x86_64-linux";
     lib = nixpkgs.lib;
-    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-    specialArgs = { inherit system; inherit inputs; };
   in {
     nixosConfigurations.nixos = lib.nixosSystem {
       inherit system;
@@ -27,14 +21,6 @@
         ./nixos
         home-manager.nixosModules.home-manager
         {
-	  nixpkgs = {
-            overlays = [
-              (final: prev: {
-                nvchad = inputs.nvchad4nix.packages."${pkgs.system}".nvchad;
-              })
-            ];
-          };
-          inherit specialArgs;
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.baldev = import ./home;
--- home/neovim.nix
+++ home/neovim.nix
@@ -1,18 +1,11 @@
-#{config, pkgs, ...}:
+{config, pkgs, ...}:
 
-#{
-#  programs.neovim = {
-#    enable = true;
-#    viAlias = true;
-#    vimAlias = true;
-#    vimdiffAlias = true;
-#    defaultEditor = true;
-#  };
-#}
-
-{ inputs, config, pkgs, ... }: {
-  imports = [
-    inputs.nvchad4nix.homeManagerModule
-  ];
-  programs.nvchad.enable = true;
+{
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+  };
 }
