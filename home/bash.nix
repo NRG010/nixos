7920262caf4549911a25049b986eed03aa8a0424
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-24 15:09:55 +0530

    $(date +%d/%m/%y\ %H:%m)

--- home/bash.nix
+++ home/bash.nix
@@ -5,7 +5,7 @@ _:
     enable = true;
     shellAliases = {
       rn = "sudo nixos-rebuild switch --flake /home/baldev/nix/#nixos";
-      gb = "git add -A; git commit -am '$(date +%d/%m/%y\\ %H:%m)'; git push";
+      gb = "git add -A; git commit -am '$(date +%d/%m/%y\ %H:%m)'; git push";
     };
   };
 }
