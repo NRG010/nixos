7a42c8edf109b55a57be34628652d3b660d73277
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-24 14:50:19 +0530

    new nixos

--- home/bash.nix
+++ home/bash.nix
@@ -4,7 +4,6 @@ _:
   programs.bash = {
     enable = true;
     shellAliases = {
-      rh = "home-manager switch --flake /home/baldev/nix/#baldev";
       rn = "sudo nixos-rebuild switch --flake /home/baldev/nix/#nixos";
     };
   };
