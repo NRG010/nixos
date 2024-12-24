d80ca112529d430a9ec9817dddacb7536141b541
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-24 20:58:06 +0530

    $(date +%d/%m/%y-%H:%m)

--- home/fish.nix
+++ home/fish.nix
@@ -12,7 +12,7 @@
     loginShellInit = """
       if status is-login
         if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-          exec startx -- -keeptty
+          Hyprland
         end
       end
       starship init fish | source
