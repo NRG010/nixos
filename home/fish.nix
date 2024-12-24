614dbc79f439980d40b738fc9e8eb5df7a788e20
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-24 21:02:22 +0530

    $(date +%d/%m/%y-%H:%m)

--- home/fish.nix
+++ home/fish.nix
@@ -7,7 +7,7 @@
       nu = "nix flake update";
       nc = "nix-collect-garbage -d";
       nr = "sudo nixos-rebuild switch --flake ~/.nix-config/#nixos";
-      gb = "git add -A;git commit -am $(date +%d/%m/%y-%H:%m);git push";
+      gb = "git add -A;git commit -am "\$(date +%d/%m/%y-%H:%m)";git push";
     };
     loginShellInit = """
       if status is-login
