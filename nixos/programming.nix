76d65d0386ec56cfb0fa038083cf4db3a110266b
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-28 21:18:52 +0530

    28/12/24-21:12

--- nixos/programming.nix
+++ nixos/programming.nix
@@ -15,15 +15,10 @@
     stylua
     lua-language-server
 
+    black
+    isort
     pyright
-    python312Full
     python314Full
-    python312Packages.pip
-    python312Packages.isort
-    python312Packages.black
-    python312Packages.numpy
-    python312Packages.pygame
-    python312Packages.matplotlib
 
     cargo
     rustc
